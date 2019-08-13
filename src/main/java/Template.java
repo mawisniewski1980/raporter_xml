65a922fd707e852974a70474ed3859158308c3d1
Marcin Wiśniewski <mawisniewski1980@gmail.com>
2019-08-13 14:28:37 +0200

    Template changed

--- src/main/java/Template.java
+++ src/main/java/Template.java
@@ -17,35 +17,36 @@ public class Template {
             "<p class=\"browsehappy\">You are using an <strong>outdated</strong> browser. Please <a href=\"#\">upgrade your browser</a> to improve your experience.</p>\n" +
             "<![endif]-->\n" +
             "\n" +
-            "<table cellspacing=\"1\" cellpadding=\"2\" border=\"0\" width=\"100%\">\n";
+            "<table cellspacing=\"1\" cellpadding=\"2\" border=\"0\" width=\"1024px\">\n";
 
-    public static String RAPORT_HEADER = "    <tr align=\"left\"><td align=\"center\">Raport z wykonania testów automatycznych: <b>%s</b><hr></td></tr>\n";
+    public static String RAPORT_HEADER = "<tr align=\"left\"><td align=\"center\">Raport z wykonania testów automatycznych: <b>%s</b><hr></td></tr>\n";
 
-    public static String RAPORT_AGREGATES = "    <tr align=\"left\"><td>Ilość testów: <b>%s</b>, <font color=\"green\">Testy pozytywne: <b>%s</b></font>, <font color=\"red\">Testy negatywne: <b>%s</b></font>, Testy pominięte: <b>%s</b>, Testy błędne: <b>%s</b><hr></td></tr>\n";
+    public static String RAPORT_AGREGATES = "<tr align=\"left\"><td>Ilość testów: <b>%s</b>, <font color=\"green\">Testy pozytywne: <b>%s</b></font>, <font color=\"red\">Testy negatywne: <b>%s</b></font>, Testy pominięte: <b>%s</b>, Testy błędne: <b>%s</b><hr></td></tr>\n";
 
-    public static String RAPORT_CLASS_SUITE = "   <tr align=\"left\"><td>Klasa: <b>%s</b>, Godzina: <b>%s</b>, Host: <b>%s</b>, Czas wykonania: <b>%s</b></td></tr>\n";
+    public static String RAPORT_CLASS_SUITE = "<tr align=\"left\"><td>Klasa: <b>%s</b>, Godzina: <b>%s</b>, Host: <b>%s</b>, Czas wykonania: <b>%s</b></td></tr>\n";
 
-    public static String RAPORT_TEST_HEADER = "    <tr align=\"left\">\n" +
+    public static String RAPORT_CLASS_AGREGATES = "<tr align=\"left\"><td>Ilość testów: <b>%s</b>, <font color=\"green\">Testy pozytywne: <b>%s</b></font>, <font color=\"red\">Testy negatywne: <b>%s</b></font>, Testy pominięte: <b>%s</b>, Testy błędne: <b>%s</b></td></tr>\n";
+
+    public static String RAPORT_TEST_HEADER = "<tr align=\"left\">" +
             "        <td>\n" +
             "            <table cellspacing=\"1\" cellpadding=\"2\" border=\"0\" width=\"100%\" >\n" +
             "                <tr bgcolor=\"#A9A9A9\">\n" +
-            "                    <td>Test</td>\n" +
-            "                    <td>Czas</td>\n" +
-            "                    <td>Typ</td>\n" +
-            "                    <td>Błąd</td>\n" +
-            "                </tr>\n" +
-            "                <tr bgcolor=\"#cecccc\">\n";
+            "                    <td>Test</td>" +
+            "                    <td>Czas</td>" +
+            "                    <td>Typ</td>" +
+            "                    <td>Błąd</td>" +
+            "                </tr>\n";
 
-    public static String RAPORT_TESTS = "                  <td>%s</td>\n" +
-            "                    <td>%s</td>\n" +
-            "                    <td>%s</td>\n" +
-            "                    <td>%s</td>\n";
+    public static String RAPORT_TESTS = "<tr bgcolor=\"#cecccc\"> " +
+            "<td>%s</td>" +
+            "<td>%s</td>" +
+            "<td>%s</td>" +
+            "<td>%s</td>" +
+            "</tr>\n";
 
-    public static String FOOTER_HTML = "               </tr>\n" +
-            "            </table>\n" +
-            "        </td>\n" +
-            "    </tr>\n" +
-            "</table>\n" +
+    public static String RAPORT_TEST_FOOTER = "</table><hr></td>\n</tr>\n";
+
+    public static String FOOTER_HTML = "</table>\n" +
             "\n" +
             "<!--<script src=\"\" async defer></script>-->\n" +
             "</body>\n" +
